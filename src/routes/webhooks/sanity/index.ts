bd102d42173fd6459cc5305f3e9eeec4eefe009d
Andy Pickler <andy.pickler@gmail.com>
2022-08-26 04:07:59 -0600

    Add Sanity webhook

---
+++ src/routes/webhooks/sanity/index.ts
@@ -0,0 +1,14 @@
+import { FastifyPluginAsync } from "fastify";
+
+const example: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
+  fastify.addHook("preValidation", (request, reply, done) => {
+    // Your code
+    done();
+  });
+
+  fastify.get("/", async function (_request, _reply) {
+    return "this is an example";
+  });
+};
+
+export default example;
